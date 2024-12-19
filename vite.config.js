9b82e40bbb39a4c282a047424188a01c4acea0fc
Alastair Waddell <awaddell@nextops.com>
2024-12-19 13:21:32 +1030

    refactor: use vite

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+
+export default defineConfig({
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        client: 'src/client.ts',
+      },
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    hmr: {
+      clientPort: 3000
+    }
+  }
+});
